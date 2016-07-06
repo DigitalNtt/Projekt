683109c2c5ab4036bfb5cf40085c3c7989a68943
DigitalNtt <luka.buchich@gmail.com>
2016-07-06 23:52:02 +0200

    dodavanje DAL layera

---
+++ Projekt.DAL/Controllers/VehicleMakesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Projekt.DAL.Models;
+using PagedList;
+
+namespace Projekt.DAL.Controllers
+{
+    public class VehicleMakesController : Controller
+    {
+        private ProjektContext db = new ProjektContext();
+
+        // GET: VehicleMakes
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var makes = from s in db.VehicleMakes
+                        select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                makes = makes.Where(s => s.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    makes = makes.OrderByDescending(s => s.Name);
+                    break;
+                default:
+                    makes = makes.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            
+            return View(makes.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: VehicleMakes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleMakes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VehicleMakes.Add(vehicleMake);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicleMake).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VehicleMake vehicleMake = db.VehicleMakes.Find(id);
+            db.VehicleMakes.Remove(vehicleMake);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Projekt.DAL/Controllers/VehicleModelsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Projekt.DAL.Models;
+using PagedList;
+
+namespace Projekt.DAL
+{
+    public class VehicleModelsController : Controller
+    {
+        private ProjektContext db = new ProjektContext();
+
+        // GET: VehicleModels
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            //var vehicleModels = db.VehicleModels.Include(v => v.VehicleMake);
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var vehicle = from s in db.VehicleModels.Include(v => v.VehicleMake)
+                select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                vehicle = vehicle.Where(s => s.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    vehicle = vehicle.OrderByDescending(s => s.Name);
+                    break;
+                default:
+                    vehicle = vehicle.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            return View(vehicle.ToPagedList(pageNumber, pageSize));
+            
+            //return View(vehicleModels.ToList());
+        }
+
+        // GET: VehicleModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Create
+        public ActionResult Create()
+        {
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "id", "Name");
+            return View();
+        }
+
+        // POST: VehicleModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,Abrv,MakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VehicleModels.Add(vehicleModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,Abrv,MakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicleModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VehicleModel vehicleModel = db.VehicleModels.Find(id);
+            db.VehicleModels.Remove(vehicleModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
